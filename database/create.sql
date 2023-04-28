8371411ae3165658dddaefd026e0be1c8be79f2f
Caleb <5322420+calebnjw@users.noreply.github.com>
2023-04-28 09:52:06 +0800

    task 1: create.sql

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,10 @@
 -- Task 1
 -- Write SQL statements in this file to create the brewery database and 
 -- populate the database with the given SQL files
+CREATE DATABASE brewerydb;
+USE brewerydb;
+SOURCE breweries.sql;
+SOURCE categories.sql;
+SOURCE styles.sql;
+SOURCE geocodes.sql;
+SOURCE beers.sql;
